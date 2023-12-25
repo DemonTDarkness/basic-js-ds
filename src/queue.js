1612196e49aa459cd941633d1925c8e441120f37
Leonid Kotov <demonthunderdarkness@gmail.com>
2023-12-25 22:47:54 +0300

    complete2

--- src/queue.js
+++ src/queue.js
@@ -14,19 +14,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  constructor () {
+    this.queue = [];
+  }
+
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code her
+    return this.queue.reverse().reduce((next, value) => ({value, next}), null);
   }
 
   enqueue(value) {
-    this.next = new ListNode(value);
-
+    this.queue.push(value)
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.queue.shift();
   }
 }
 
